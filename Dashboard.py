84032a0bff542bc69792857cb4f48441e320b232
Admin <jason8417@icloud.com>
2022-11-05 12:24:17 -0400

    add empty constructor

--- Dashboard.py
+++ Dashboard.py
@@ -4,16 +4,20 @@ import pandas as pd
 
 # parent/super class
 class Investment_Dashboard:
-    pass
+    def __init__(self):
+        pass
 
 # child class
 class futures(Investment_Dashboard):
-    pass
+    def __init__(self):
+        pass
 
 # child class
 class options(Investment_Dashboard):
-    pass
+    def __init__(self):
+        pass
 
 # child class
 class swaps(Investment_Dashboard):
-    pass
+    def __init__(self):
+        pass
