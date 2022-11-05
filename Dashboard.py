711d8a001fe2d060ed4dfadc2e6bd53d5daa7bc8
Jason Ang <104236373+Jason-B-Ang@users.noreply.github.com>
2022-11-05 10:57:41 -0400

    Add files via upload
    First template test of py dashboard file

---
+++ Dashboard.py
@@ -0,0 +1,11 @@
+class Investment_Dashboard:
+    pass
+
+class futures(Investment_Dashboard):
+    pass
+
+class options(Investment_Dashboard):
+    pass
+
+class swaps(Investment_Dashboard):
+    pass
