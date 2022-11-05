f00d5b77817d6235b93b5558681f74788f6ebb4f
Admin <jason8417@icloud.com>
2022-11-05 11:46:40 -0400

    added two librarys

---
+++ Dashboard.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+# parent/super class
+class Investment_Dashboard:
+    pass
+
+# child class
+class futures(Investment_Dashboard):
+    pass
+
+# child class
+class options(Investment_Dashboard):
+    pass
+
+# child class
+class swaps(Investment_Dashboard):
+    pass
